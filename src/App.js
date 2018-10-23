94237facab4e768834fb16fe544a52f84e53a060
Stuart B <stuart.busilla@gmail.com>
2018-10-23 13:27:07 -0500

    Add Clarifai api and events to detect faces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '7e59c084dc4b4f35bfeaf4df34cb3755'
+});
 
 const particlesOptions = {
 	particles: {
@@ -19,21 +25,46 @@ const particlesOptions = {
 }
 
 class App extends Component {
-  render() {
-    return (
-    	<div className="App">
-    		<Particles className='particles'
-              params={particlesOptions}
-            />
-      		<Navigation />
-      		<Logo />
-      		<Rank />
-      		<ImageLinkForm />
-      		{/*
-      			<FaceRecognition />*/}
-     	</div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	}
+
+	onButtonSubmit = () => {
+		this.setState({imageUrl: this.state.input});
+		app.models.predict(
+			Clarifai.FACE_DETECT_MODEL,
+			this.state.input)
+		.then(
+		    function(response) {
+		    	console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+		    },
+		    function(err) {
+		      // there was an error
+		    }
+	    );
+	}
+
+	render() {
+	return (
+		<div className="App">
+			<Particles className='particles' params={particlesOptions} />
+	  		<Navigation />
+	  		<Logo />
+	  		<Rank />
+	  		<ImageLinkForm onInputChange={this.onInputChange}
+	  			onButtonSubmit={this.onButtonSubmit} />
+	  		{<FaceRecognition imageUrl={this.state.imageUrl} />}
+	 	</div>
+	);
+	}
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FaceRecogntion = ({ imageUrl }) => {
+	return (
+		<div className='center ma'>
+			<div className='absolute mt2'>
+				<img alt='' src={imageUrl} width='500px' height='auto' />
+			</div>
+		</div>
+	)
+}
+
+export default FaceRecogntion;

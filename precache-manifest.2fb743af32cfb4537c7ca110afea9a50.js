359deeb2f3bfd9e1c2b03764422039868fbaa741
Stuart B <stuart.busilla@gmail.com>
2018-11-02 21:24:31 -0500

    Updates

---
+++ precache-manifest.2fb743af32cfb4537c7ca110afea9a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eff21dfb82952397757",
+    "url": "/smart-brain/static/js/runtime~main.0eff21df.js"
+  },
+  {
+    "revision": "6e5d18e5a59fa4df440a",
+    "url": "/smart-brain/static/js/main.6e5d18e5.chunk.js"
+  },
+  {
+    "revision": "42fb7e6e4e60a1956f16",
+    "url": "/smart-brain/static/js/1.42fb7e6e.chunk.js"
+  },
+  {
+    "revision": "6e5d18e5a59fa4df440a",
+    "url": "/smart-brain/static/css/main.c64da9f9.chunk.css"
+  },
+  {
+    "revision": "42fb7e6e4e60a1956f16",
+    "url": "/smart-brain/static/css/1.648523f1.chunk.css"
+  },
+  {
+    "revision": "a84ddd81af502201b21593aea336099e",
+    "url": "/smart-brain/index.html"
+  }
+];
